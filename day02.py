1a64bbde0ec65b546c2c952d3f044399d1d1d808
Victor <victor.kryukov@gmail.com>
2020-12-01 21:15:36 -0800

    Day 2 solution

---
+++ day02.py
@@ -0,0 +1,27 @@
+from utils import *
+
+
+def parse_line(s):
+    region, letter, password = s.split()
+    lo, hi = region.split('-')
+    lo, hi = int(lo), int(hi)
+    letter = letter[0]
+    return lo, hi, letter, password
+
+
+def valid(s):
+    lo, hi, letter, password = parse_line(s)
+    return lo <= password.count(letter) <= hi
+
+
+def valid2(s):
+    lo, hi, letter, password = parse_line(s)
+    return count([password[lo - 1] == letter, password[hi - 1] == letter]) == 1
+
+
+def test_part1():
+    assert count(read_lines('inputs/day02.txt'), valid) == 580
+
+
+def test_part2():
+    assert count(read_lines('inputs/day02.txt'), valid2) == 611
